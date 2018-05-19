5f03d9730ed0e288f00e81a42cac9578a35577a9
Vu Xuan Loc <vuxuanloc@me.com>
2018-05-19 19:01:05 +0700

    Add source code

---
+++ 31151020673_LeThanhTruc/31151020673_LeThanhTruc/Bai_1/js_bai1.js
@@ -0,0 +1,30 @@
+﻿function random(min, max) {
+    min = 1;
+    max = parseInt(document.getElementsByName("textBox2")[0].value);
+    val = Math.floor(Math.random() * (max - min + 1)) + min;
+    document.getElementsByName("textBoxVal")[0].value = val;
+}
+function sort(min, max) {
+
+    min = 1;
+    max = parseInt(document.getElementsByName("textBox2")[0].value);
+    document.getElementsByName("textBoxVal")[0].value = "";
+    var ok = 1; r = new Array(max)
+
+    for (var i = 1; i <= max; i++) {
+        r[i] = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    for (var i = max; i >= 1; i--) {
+        for (var j = max; j >= 1; j--) {
+            if ((i != j) && (r[i] == r[j])) ok = 0;
+        }
+    }
+    if (ok) {
+
+        for (var k = 1; k <= max; k++) {
+            output = "";
+            output += + r[k] + " ";
+            document.getElementsByName("textBoxVal")[0].value += output;
+        }
+    } else sort(min, max);
+}
---
+++ 31151020673_LeThanhTruc/31151020673_LeThanhTruc/Bai_3/js_Bai3.js
@@ -0,0 +1,6 @@
+﻿function giamgia() {
+    val = parseInt(document.getElementsByName("input")[0].value);
+    if (val == 31151020673) { alert(" Bạn được giảm 5% ");  }
+    else { alert(" Mã giảm giá không hợp lệ "); }
+    
+}
